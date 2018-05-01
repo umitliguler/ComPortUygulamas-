17fb7885eb108a0db812456c3b26ca876805705b
umitliguler <info.umitguler@gmail.com>
2018-05-01 20:43:31 +0300

    first

---
+++ ComPortUygulamasi/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace ComPortUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.send = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.read = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.OpenPort = new System.Windows.Forms.Button();
+            this.ClosePort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.refleshButton = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.status_label = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(52, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(141, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "4800",
+            "9600",
+            "19200",
+            "115200",
+            "480500"});
+            this.comboBox2.Location = new System.Drawing.Point(52, 108);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(141, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.send);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(52, 158);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(169, 173);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gönderilecek Bilgiler";
+            // 
+            // send
+            // 
+            this.send.AccessibleRole = System.Windows.Forms.AccessibleRole.Cursor;
+            this.send.Enabled = false;
+            this.send.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.send.Location = new System.Drawing.Point(72, 132);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(91, 30);
+            this.send.TabIndex = 1;
+            this.send.Text = "Gönder";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(3, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 107);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.read);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(242, 158);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(173, 173);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Alınan Bilgiler";
+            // 
+            // read
+            // 
+            this.read.Enabled = false;
+            this.read.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.read.Location = new System.Drawing.Point(81, 133);
+            this.read.Name = "read";
+            this.read.Size = new System.Drawing.Size(86, 29);
+            this.read.TabIndex = 1;
+            this.read.Text = "Oku..";
+            this.read.UseVisualStyleBackColor = true;
+            this.read.Click += new System.EventHandler(this.read_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(6, 19);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(162, 107);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // OpenPort
+            // 
+            this.OpenPort.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.OpenPort.Location = new System.Drawing.Point(461, 225);
+            this.OpenPort.Name = "OpenPort";
+            this.OpenPort.Size = new System.Drawing.Size(130, 38);
+            this.OpenPort.TabIndex = 5;
+            this.OpenPort.Text = "Portu Aç";
+            this.OpenPort.UseVisualStyleBackColor = true;
+            this.OpenPort.Click += new System.EventHandler(this.OpenPort_Click);
+            // 
+            // ClosePort
+            // 
+            this.ClosePort.Enabled = false;
+            this.ClosePort.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ClosePort.Location = new System.Drawing.Point(461, 280);
+            this.ClosePort.Name = "ClosePort";
+            this.ClosePort.Size = new System.Drawing.Size(130, 40);
+            this.ClosePort.TabIndex = 6;
+            this.ClosePort.Text = "Portu Kapat";
+            this.ClosePort.UseVisualStyleBackColor = true;
+            this.ClosePort.Click += new System.EventHandler(this.ClosePort_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(52, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 22);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ComPortlar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(48, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 22);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "BaudRate";
+            // 
+            // refleshButton
+            // 
+            this.refleshButton.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.refleshButton.Location = new System.Drawing.Point(461, 164);
+            this.refleshButton.Name = "refleshButton";
+            this.refleshButton.Size = new System.Drawing.Size(130, 39);
+            this.refleshButton.TabIndex = 9;
+            this.refleshButton.Text = "Portları Yenile";
+            this.refleshButton.UseVisualStyleBackColor = true;
+            this.refleshButton.Click += new System.EventHandler(this.refleshButton_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.status_label);
+            this.groupBox3.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox3.Location = new System.Drawing.Point(242, 21);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(173, 63);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Bağlantı Durumu";
+            // 
+            // status_label
+            // 
+            this.status_label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.status_label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.status_label.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.status_label.Location = new System.Drawing.Point(5, 25);
+            this.status_label.Name = "status_label";
+            this.status_label.Size = new System.Drawing.Size(163, 26);
+            this.status_label.TabIndex = 26;
+            this.status_label.Text = "Bağlantı Bekleniyor..";
+            this.status_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(242, 105);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(173, 24);
+            this.progressBar1.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(5, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 22);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Saat:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(5, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 22);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Tarih:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox4.Location = new System.Drawing.Point(439, 21);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(179, 47);
+            this.groupBox4.TabIndex = 20;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Saat";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.label5);
+            this.groupBox5.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox5.Location = new System.Drawing.Point(439, 83);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(179, 46);
+            this.groupBox5.TabIndex = 21;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Tarih";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 353);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.refleshButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ClosePort);
+            this.Controls.Add(this.OpenPort);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Serial Communication";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button read;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button OpenPort;
+        private System.Windows.Forms.Button ClosePort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button refleshButton;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label status_label;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+    }
+}
+
---
+++ ComPortUygulamasi/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace ComPortUygulamasi
+{
+    public partial class Form1 : Form
+    {
+
+        public static Int32 AlınanData = 0;
+        public static Byte[] Buffer = new byte[40];
+        public static string[] ports;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Mevcut portları görüntüle
+            getAvaiblePorts();
+            //Arka plan rengini ayarla
+            this.BackColor = Color.Silver;
+            //this.BackColor = Color.FloralWhite;
+
+        }
+
+
+        //mevcut portları combobox a gönderdik..
+        void getAvaiblePorts()
+        {
+
+             ports = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+        }
+
+        //Baudrate için formda items;collection tıklanır ve değerler alt alta eklenir. 
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        //form menususunden readonly true yaptık
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Portu Aç
+        private void OpenPort_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.Text == "" || comboBox2.Text == "")
+                {
+                    //textBox2.Text = "Lütfen port ayarlamalarını yapınız";                    
+                    MessageBox.Show("Lütfen port ayarlamalarını yapınız!", "Bilgilendirme Penceresi");
+
+                }
+                else
+                {
+                    serialPort1.PortName = comboBox1.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
+                    serialPort1.StopBits = StopBits.One;
+                    serialPort1.Parity = Parity.None;
+                    serialPort1.Open();
+                    progressBar1.Value = 100;
+                    send.Enabled = true;
+                    read.Enabled = true;
+                    textBox1.Enabled = true;
+                    OpenPort.Enabled = false;
+                    ClosePort.Enabled = true;
+                    status_label.Text = string.Format("{0} Bağlandı.", ports);
+                    textBox2.Text = "";
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+
+                textBox2.Text = "Yetkilendirilmemiş Erişim";
+            }
+        }
+
+        //Portu kapat
+        private void ClosePort_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            progressBar1.Value = 0;
+            send.Enabled = false;
+            read.Enabled = false;
+            OpenPort.Enabled = true;
+            ClosePort.Enabled = false;
+            textBox1.Enabled = false;
+            status_label.Text = "Bağlantı yok";
+            status_label.ForeColor = Color.FromArgb(50, 0, 0);
+
+        }
+
+
+        //bilgileri yolla
+        private void send_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Write(textBox1.Text + Environment.NewLine);
+                    textBox1.Clear();
+                }
+
+            }
+            catch (Exception Ex)
+            {
+
+                MessageBox.Show(Ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            //read_Click(sender, e);
+
+            //if (Buffer[0] == 0x80)
+            //{
+            //    status_label.Text = "Yazma başarılı";
+            //    status_label.ForeColor = Color.FromArgb(0, 50, 0);
+            //}
+            //else
+            //{
+            //    status_label.Text = "Yazma Başarısız";
+            //    status_label.ForeColor = Color.FromArgb(50, 0, 0);
+            //}
+        }
+
+        //gelen bilgileri oku
+        private void read_Click(object sender, EventArgs e)
+        {
+            //delay_ms(100);
+
+            textBox2.Text = "";
+
+            if (!serialPort1.IsOpen) return;
+
+            try
+            {
+                textBox2.Text = serialPort1.ReadExisting();
+            }
+
+            catch (TimeoutException)
+            {
+                textBox2.Text = "Timeout Exception";
+            }
+
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+                //delay_ms(100);
+        }
+
+
+
+        void delay_ms(int x)
+        {
+            System.Threading.Thread.Sleep(x);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+            label4.Text = DateTime.Now.ToLongTimeString();
+            label5.Text = DateTime.Now.ToLongDateString();
+
+        }
+
+        //Saat/Tarih için timer
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            label4.Text = DateTime.Now.ToLongTimeString();
+            timer1.Start();
+        }
+
+        //Portları yenile
+        private void refleshButton_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            comboBox1.Text = "";
+            getAvaiblePorts();
+        }
+
+    }
+    }
